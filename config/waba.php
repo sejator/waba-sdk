8b8939e5553d2d417bca7258d6c1e0f95840be5f
Sejator Dev <sejatordev@gmail.com>
2026-05-15 03:03:29 +0700

    update: hapus config yang tidak di pakai

--- config/waba.php
+++ config/waba.php
@@ -1,14 +1,8 @@
 <?php
 
 return [
-    'url' => env('WABA_URL', ''),
-    'token' => env('WABA_TOKEN'),
-    'otp_expiry' => env('WABA_OTP_EXPIRY', 5), // in minutes
-    'internal' => [
-        'key' => env('WABA_INTERNAL_KEY', ''),
-        'gateway_url' => env('WABA_GATEWAY_URL', ''),
-    ],
     'meta' => [
+        'app_id' => env('META_APP_ID', ''),
         'base_url' => env('META_BASE_URL', 'https://graph.facebook.com/v23.0'),
         'access_token' => env('META_ACCESS_TOKEN', ''),
     ]
