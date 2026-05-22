5119bc6518d57f80b9efb8ffb273567dbefeb446
Sejator Dev <sejatordev@gmail.com>
2026-05-22 17:11:51 +0700

    fix: generate url embed tidak sesuai

--- src/Support/Embedded/OAuthUrlGenerator.php
+++ src/Support/Embedded/OAuthUrlGenerator.php
@@ -12,24 +12,34 @@ class OAuthUrlGenerator
 
         $state = Str::uuid()->toString();
 
+        $extras = [
+            'sessionInfoVersion' => '3',
+            'version' => 'v4',
+        ];
+
+        $redirect = config(
+            'waba.embedded.redirect_uri'
+        );
+
+        $query = http_build_query([
+            'display' => 'popup',
+            'client_id' => config(
+                'waba.meta.app_id'
+            ),
+            'redirect_uri' => $redirect,
+            'fallback_redirect_uri' => $redirect,
+            'config_id' => config(
+                'waba.embedded.config_id'
+            ),
+            'response_type' => 'code',
+            'override_default_response_type' => true,
+            'state' => $state,
+            'extras' => json_encode($extras),
+        ]);
         $version = config(
             'waba.embedded.oauth_version',
             'v23.0'
         );
-
-        $query = http_build_query([
-            'client_id' => config('waba.meta.app_id'),
-            'redirect_uri' => config(
-                'waba.embedded.redirect_uri'
-            ),
-
-            'scope' => 'business_management,whatsapp_business_management,whatsapp_business_messaging',
-            'response_type' => 'code',
-            'state' => $state,
-            'config_id' => config(
-                'waba.embedded.config_id'
-            ),
-        ]);
 
         return [
             'state' => $state,
@@ -41,18 +51,21 @@ class OAuthUrlGenerator
     protected function ensureConfigured(): void
     {
         if (!config('waba.meta.app_id')) {
+
             throw new \RuntimeException(
                 'META_APP_ID is not configured.'
             );
         }
 
         if (!config('waba.embedded.config_id')) {
+
             throw new \RuntimeException(
                 'META_CONFIGURATION_ID is not configured.'
             );
         }
 
         if (!config('waba.embedded.redirect_uri')) {
+
             throw new \RuntimeException(
                 'META_REDIRECT_URI is not configured.'
             );
